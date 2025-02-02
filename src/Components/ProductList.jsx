c95d85153148681978d54ad734c454eab150cca0
amarjeet207 <amangat7017@gmail.com>
2025-02-03 02:10:46 +0530

    changes in productList

--- src/Components/ProductList.jsx
+++ src/Components/ProductList.jsx
@@ -4,7 +4,7 @@ const ProductList = (props) => {
   return (
     <div>
       <div className="flex flex-col items-center md:w-36 sm:w-16 h-fit cursor-pointer">
-        <div className=" sm:w-10 sm:h-10 md:w-28 md:h-28  rounded-full overflow-hidden">
+        <div className=" sm:w-10 sm:h-10 md:w-28 md:h-28 rounded-full overflow-hidden">
           <img
             src={props.src}
             className="w-full h-full object-cover rounded-full"
